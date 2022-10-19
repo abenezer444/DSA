ab7f102485b679eece03b9e647d8aca16f7a2a6d
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2022-10-19 17:57:02 +0300

    Time: 81 ms (75.01%), Space: 16.8 MB (47.15%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -5,13 +5,24 @@ class Solution(object):
         :type newInterval: List[int]
         :rtype: List[List[int]]
         """
-        intervals.append(newInterval)
-        intervals.sort()
+        if not intervals:
+            return [newInterval]
+        n=len(intervals)
+        for i in range(n):
+            if newInterval[0]<=intervals[i][0]:
+                intervals.insert(i,newInterval)
+                break
+            else:
+                intervals.append(newInterval)
+                    
+      
         res=[intervals[0]]
         for i in intervals[1:]:
             if i[0]<=res[-1][1]:
-                res[-1][1]=max(res[-1][1],i[1])
+                res[-1][1]=max(i[1],res[-1][1])
             else:
                 res.append(i)
         return res
+                
+                    
         
