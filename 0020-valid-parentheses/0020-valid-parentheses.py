ca461ab79bdc7b0b1e289805cf5d18ae5e44a224
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2022-10-24 20:52:22 +0300

    Time: 33 ms (50.93%), Space: 13.6 MB (30.01%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack=[]
+        opening=['(','[','{']
+        #add openings to the stack
+        #if not opening, check if current punc value and the end of the stack are the same
+        #if not same return false
+        #else pop stack
+        pmap={')':'(',']':'[','}':'{'}
+        for i in s:
+            if i in opening:
+                stack.append(i)
+            else:
+                if  stack and pmap[i]==stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+        return not bool(stack)
+        
