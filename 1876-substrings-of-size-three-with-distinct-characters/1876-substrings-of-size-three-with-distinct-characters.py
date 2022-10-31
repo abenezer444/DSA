c01bbfe1f7a052bf047ae11b9e82ca64c2e886bf
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2022-10-31 20:38:27 +0300

    Time: 61 ms (5.53%), Space: 13.6 MB (9.22%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
@@ -4,23 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        setAlpha=set()
-        counter=0
-        left=0
-        right=0
-        while right<len(s):
-            #expand, check if char is in my set
-            while right<len(s) and s[right] in setAlpha:
-                setAlpha.remove(s[left])
-                left+=1
-            if right<len(s) and s[right] not in setAlpha:
-                setAlpha.add(s[right])
-            
-            if len(setAlpha)==3:
-                    counter+=1
-                    setAlpha.remove(s[left])
-                    left+=1
-            
-            right+=1
-        return counter
-        
+        count=0
+        for i in range(len(s)-2):
+            string=s[i]
+            for j in range(i+1,i+3):
+                if s[j] in string:
+                    break
+                string+=s[j]
+            if len(string)==3:
+                count+=1
+        return count
+                
