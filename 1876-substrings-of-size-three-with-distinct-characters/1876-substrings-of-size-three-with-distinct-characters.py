2cdafd76fa3fee6f4581f2dfc11427cb43134e93
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2022-10-31 16:44:13 +0300

    Time: 28 ms (75.12%), Space: 13.6 MB (27.65%) - LeetHub

---
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def countGoodSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        setAlpha={1}
+        counter=0
+        left=0
+        right=0
+        while right<len(s):
+            #expand, check if char is in my set
+            while right<len(s) and s[right] in setAlpha:
+                setAlpha.remove(s[left])
+                left+=1
+            if right<len(s) and s[right] not in setAlpha:
+                setAlpha.add(s[right])
+            
+            if len(setAlpha)==4:
+                    counter+=1
+                    setAlpha.remove(s[left])
+                    left+=1
+            
+            right+=1
+        return counter
+        
