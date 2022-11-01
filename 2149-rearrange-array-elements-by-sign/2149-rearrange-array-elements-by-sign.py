e4aaa1a76f01683de290f18a2672b24add6b6e2a
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2022-11-01 10:13:34 +0300

    Time: 3625 ms (15.83%), Space: 46.2 MB (69.07%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -4,16 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        output=[]
-        positives=[]
-        negatives=[]
+        output=[0]*len(nums)
+        p=0
+        n=1
         for i in nums:
-            if i<0:
-                negatives.append(i)
+            if i>0:
+                output[p]=i
+                p+=2
             else:
-                positives.append(i)
-        for i in range(len(nums)//2):
-            output.append(positives[i])
-            output.append(negatives[i])
+                output[n]=i
+                n+=2
         return output
+            
+        
         
