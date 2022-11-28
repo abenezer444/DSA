d1fe740c3866f93dda54e3dc39d38bff303998f7
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2022-11-28 12:18:13 +0300

    Time: 79 ms (95.22%), Space: 44.7 MB (88.93%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -3,10 +3,9 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    var len=nums.length
-    for (var i=0; i<len;i+=1){
-        nums.push(nums[i]);
-    }
+    var len=nums
+    nums.push(...len)
+    
     return nums
     
 };
