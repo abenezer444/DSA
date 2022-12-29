94dc0585c47725865cc126976dc959c9275875f6
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2022-12-29 11:11:05 +0300

    Time: 80 ms (92.38%), Space: 13.8 MB (54.12%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -9,13 +9,19 @@ class Solution(object):
         maxFreq=0
         left=0
         countMap={}
+        
         for right in range(len(s)):
+            
             countMap[s[right]]=1+countMap.get(s[right],0)
             maxFreq=max(countMap[s[right]],maxFreq)
+            
             while k+maxFreq <right-left+1 and left<len(s):
+                
                 countMap[s[left]]-=1
                 left+=1
+                
             ans=max(ans,right-left+1)
+            
         return ans
             
                 
