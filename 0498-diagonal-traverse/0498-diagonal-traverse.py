ea207dc303946347edeed119c0df51d38b094dec
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2023-01-06 14:52:45 +0300

    Time: 206 ms (73.56%), Space: 17 MB (89.42%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -3,11 +3,11 @@ class Solution:
         asDiagonal=[]
         rows=len(mat)
         cols=len(mat[0])
-        diagonals=[[] for _ in range(rows+cols-1)]
+        diagonals=[[] for _ in range(rows+cols-1)] #the number of diagonals has this mathematical relationship 
         
         for row in range(rows):
             for col in range(cols):
-                diagonals[col+row].append(mat[row][col])
+                diagonals[col+row].append(mat[row][col]) #summing the col and the row gives the index of the diagonal on which htis element will end up
         
         for index in range(len(diagonals)):
             if index%2==0:
