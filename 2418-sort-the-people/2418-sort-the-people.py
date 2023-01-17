f2ed27cff99e94f87c1ac5fc4b2f50ba9772e301
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2023-01-17 18:38:07 +0300

    Time: 1572 ms (5.18%), Space: 14 MB (55.96%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -7,9 +7,15 @@ class Solution(object):
         """
         pair=[(heights[i],names[i]) for i in range(len(names))]
         for i in range(len(pair)):
+            flag=True
             for j in range(len(pair)-1):
                 if pair[j]<pair[j+1]:
                     pair[j],pair[j+1]=pair[j+1],pair[j]
+                    flag=False
+            if flag:
+                break
+                
+                    
         ans=[]
         for num in pair:
             ans.append(num[1])
