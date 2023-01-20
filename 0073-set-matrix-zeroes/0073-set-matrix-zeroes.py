f6616541230eb068aa9f4bb061414d50a92afafa
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2023-01-20 16:51:25 +0300

    Time: 137 ms (66.78%), Space: 14.8 MB (64.10%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -5,19 +5,16 @@ class Solution:
         """
         length=len(matrix)
         width=len(matrix[0])
-        rows=set()
-        columns=set()
-        for i in range(length):
-            for j in range(width):
-                if matrix[i][j]==0:
-                    rows.add(i)
-                    columns.add(j)
-        for row in rows:
-            for j in range(width):
-                matrix[row][j]=0
-        for column in columns:
-            for j in range(length):
-                matrix[j][column]=0
-        
-        
+        zeros=[]
+        for row in range(length):
+            for col in range(width):
+                if matrix[row][col]==0:
+                    zeros.append([row,col])
+        for zero in zeros:
+            rows=zero[0]
+            cols=zero[1]
+            for row in range(length):
+                matrix[row][cols]=0
+            for col in range(width):
+                matrix[rows][col]=0
         
