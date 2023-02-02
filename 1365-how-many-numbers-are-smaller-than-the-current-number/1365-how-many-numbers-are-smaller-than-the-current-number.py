9d7e0f0a273e8765ec3ed48220e5391f7ea7070a
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2023-02-02 03:54:48 +0300

    Time: 73 ms (76.55%), Space: 14 MB (7.22%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,19 +1,22 @@
-class Solution(object):
-    def smallerNumbersThanCurrent(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        #sort the array and save the index of the first occurence of any unique element.
-        table={}
-        lst=sorted(nums)
-        n=len(nums)
-        res=[]
-        for i in range(n):
-            if lst[i] not in table:
-                table[lst[i]]=i
-        for i in range(n):
-            res.append(table[nums[i]])
-        return res
-                
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        numbers=nums.copy()
+        numbers.sort()
+        def leftFinder(target):
+            left=0
+            right=len(nums)-1
+            while left<=right:
+                mid=(left+right)//2
+                if numbers[mid]==target:
+                    temp=mid
+                    right=mid-1
+                elif numbers[mid]>target:
+                    right=mid-1
+                else:
+                    left=mid+1
+            return temp
+        for idx,num in enumerate(nums):
+            nums[idx]=leftFinder(num)
+        return nums
+            
         
