3ecf35d86b44de8cfe2c9df2e937465ed48197b2
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2023-02-02 04:40:59 +0300

    Time: 33 ms (68.37%), Space: 13.5 MB (18.67%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
@@ -6,38 +6,8 @@ class Solution(object):
         :rtype: List[int]
         """
         nums.sort()
-        res=-1
-        l=0
-        n=len(nums)
-        r=n-1
-        #find the left most occurence
-        while l<=r:
-            mid=(l+r)/2
-            if nums[mid]>target:
-                r=mid-1
-            elif nums[mid]<target:
-                l=mid+1
-            else:
-                res=mid
-                r=mid-1
-        left=res
-        print(left)
-        res=-1
-        l=0
-        r=n-1
-        while l<=r:
-            mid=(l+r)/2
-            if nums[mid]>target:
-                r=mid-1
-            elif nums[mid]<target:
-                l=mid+1
-            else:
-                res=mid
-                l=mid+1
-        right=res
-        print(right)
-        if nums[res]==target:
-            return [i for i in range(left,right+1)]
-        return []
-        
-                
+        output=[]
+        for i in range(len(nums)):
+            if nums[i]==target:
+                output.append(i)
+        return output
