1a16299f384c7a2fe9d2d8c2f82bcb7c10677b1a
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2023-02-21 16:13:03 +0300

    Time: 64 ms (87.94%), Space: 13.9 MB (35.09%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,34 +5,65 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        stack = []
-        rem = []
+        def createNode(val):
+            return ListNode(val)
+        pre1=ListNode(0,l1)
+        pre2=ListNode(0,l2)
         
+        ans=ListNode()
+        ref, carry,prevRaw =ans, 0,0
         while l1 and l2:
-            carry = rem.pop() if rem else 0
-            stack.append((l1.val+l2.val+carry)%10)
-            if (l1.val+l2.val+carry)//10 == 1:
-                rem.append(1)
-            l1 = l1.next
-            l2 = l2.next
+            carry=1 if prevRaw>9 else 0
+            raw=l1.val+l2.val+carry
+            # carry = 0
+            if raw>9:
+                node=createNode(raw%10)
+            else:
+                node=createNode(raw)
+            l1=l1.next
+            l2=l2.next
+            pre1=pre1.next
+            pre2=pre2.next
+            ans.next=node
+            ans=ans.next
+            carry=1 if raw>9 else 0
+            prevRaw = raw
+
+
         while l1:
-            carry = rem.pop() if rem else 0
-            stack.append((l1.val + carry)%10)
-            if(l1.val + carry)//10:
-                rem.append(1)
-            l1 = l1.next
+            raw = 0
+            if carry:
+                raw =carry
+            raw +=l1.val
+            carry = 1 if raw > 9 else 0
+            if raw>9:
+                node=createNode(raw%10)
+            else:
+                node=createNode(raw)
+            l1=l1.next
+            ans.next=node
+            ans=ans.next
         while l2:
-            carry = rem.pop() if rem else 0
-            stack.append((l2.val + carry)%10)
-            if(l2.val + carry)//10:
-                rem.append(1)
-            l2 = l2.next
-        stack.extend(rem)
-        dummy = ListNode()
-        tail = dummy
-        for num in stack:
-            tail.next = ListNode(num)
-            tail = tail.next
-        return dummy.next
-        
+            raw = 0
+            if carry:
+                raw =carry
+            
+            raw +=l2.val
+            carry = 1 if raw > 9 else 0
+            if raw>9:
+                node=createNode(raw%10)
+            else:
+                node=createNode(raw)
+            l2=l2.next
+            ans.next=node
+            ans=ans.next
+        if carry:
+            node=createNode(1)
+            ans.next=node
+            ans=ans.next
+            
+            
+            
+            
+        return ref.next
                 
