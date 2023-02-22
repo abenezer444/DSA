ec0f7e31462e803ef6a24553eaddb7be2e65d104
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2023-02-22 10:40:04 +0300

    Time: 1492 ms (19.75%), Space: 25.6 MB (78.18%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -4,10 +4,19 @@ class Solution:
         for index in range(k):
             curSum+=nums[index]
         maxSum=curSum
-        
-        for index in range(1,len(nums)-k+1):
-            curSum-=nums[index-1]
-            curSum+=nums[index+k-1]
+        start=0
+        for index in range(k,len(nums)):
+            curSum-=nums[start]
+            curSum+=nums[index]
+            start+=1
             maxSum=max(maxSum,curSum)
+        
+            
+            
+        
+        # for index in range(1,len(nums)-k+1):
+        #     curSum-=nums[index-1]
+        #     curSum+=nums[index+k-1]
+        #     maxSum=max(maxSum,curSum)
         return maxSum/k
         
