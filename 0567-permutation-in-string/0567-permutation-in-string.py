3ee6f2b28a8153dc6feb502e381971522bff4fd1
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2023-02-23 16:38:03 +0300

    Time: 62 ms (89.84%), Space: 13.8 MB (93.05%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,8 +1,26 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         asDict=Counter(s1)
-        for index in range(len(s2)):
-            if asDict==Counter(s2[index:index+len(s1)]):
+        sliding=defaultdict(int)
+        if len(s2)<len(s1):
+            return False
+        for index in range(len(s1)):
+            sliding[s2[index]]+=1
+        
+        if asDict==sliding:
+            
+                return True
+            
+        left=0
+        for index in range(len(s1),len(s2)):
+            sliding[s2[index]]+=1
+            sliding[s2[left]]-=1
+            if not sliding[s2[left]]:
+                del sliding[s2[left]]
+                
+            left+=1
+            if asDict==sliding:
+            
                 return True
         return False
         
