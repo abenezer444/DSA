cc771f5506bfb2a37251a119d66de664b08e2f68
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2023-03-01 09:57:02 +0300

    Time: 27 ms (90.69%), Space: 13.8 MB (56.28%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,4 +1,14 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x**n
+        if n<0:
+            x = 1/x
+            n = -n
+        if n==0:
+            return 1
+        if not n % 2:
+            temp=Solution.myPow(self,x,n/2)
+            ans= temp*temp
+            return ans
+        else:
+            return x*(self.myPow(x,n-1))
         
