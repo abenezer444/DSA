ebc1af5ac0368646d7cff10e131998ca17a6a510
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2023-03-03 09:31:09 +0300

    Time: 164 ms (91.64%), Space: 15.3 MB (75.64%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -4,24 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        #ans[2,3,4,-1,-1]
-        #[(4,3),(3,4)]
-        #while len(stack)>1:
-        # for idx,num in enumerate(nums):
-            # while stack and stack[-1][0]<num:
-            #     ans[stack.pop()[1]]=num
-            # stack.append([num,idx])
-            
-        n=2*len(nums)
-        
         ans=[-1]*len(nums)
         stack=[]
-        for i in range(n):
-            idx=i%len(nums)
-            while stack and stack[-1][0]<nums[idx]:
-                ans[stack.pop()[1]]=nums[idx]
-            stack.append([nums[idx],idx])
-            
+        for idx,num in enumerate(nums):
+            while stack and stack[-1][0]<num:
+                ans[stack.pop()[1]]=num
+            stack.append([num,idx])
+        for idx,num in enumerate(nums):
+            while stack and stack[-1][0]<num:
+                ans[stack.pop()[1]]=num
+        
       
         return ans
             
