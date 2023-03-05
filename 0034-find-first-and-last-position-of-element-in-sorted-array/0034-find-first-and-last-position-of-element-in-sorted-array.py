c9f88d3fc9d0724cbc2bb90434224ae322ec7036
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2023-03-05 15:14:04 +0300

    Time: 88 ms (66.87%), Space: 15.4 MB (39.54%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,38 +1,16 @@
-class Solution(object):
-    def searchRange(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        res=-1
-        l=0
-        n=len(nums)
-        r=n-1
-        #find the left most occurence
-        while l<=r:
-            mid=(l+r)/2
-            if nums[mid]>target:
-                r=mid-1
-            elif nums[mid]<target:
-                l=mid+1
-            else:
-                res=mid
-                r=mid-1
-        left=res
-        print(left)
-        res=-1
-        l=0
-        r=n-1
-        while l<=r:
-            mid=(l+r)/2
-            if nums[mid]>target:
-                r=mid-1
-            elif nums[mid]<target:
-                l=mid+1
-            else:
-                res=mid
-                l=mid+1
-        right=res
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if not nums:
+            return [-1,-1]
+    
+            
+        left = bisect_left(nums,target,0,len(nums)-1)
+        right = bisect_right(nums,target,0,len(nums))
+        if nums[right-1] == target:
+            right -= 1
+        else:
+            right = -1
+        if nums[left]!=target:
+            left = -1
+            
         return [left,right]
-        
