883496966d2dcd18807a000ecaa3b1f4818172a8
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2023-03-15 10:25:58 +0300

    Time: 21 ms (27.96%), Space: 13.5 MB (41.94%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -1,12 +1,14 @@
-class Solution:
-    def kthGrammar(self, n: int, k: int) -> int:
-        if n ==1 or k==1:
+class Solution(object):
+    def kthGrammar(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        if n == 1 or k == 1:
             return 0
-        ans = self.kthGrammar(n-1,ceil(k/2))
-        if k%2 == 0:
-            if ans == 0:
-                ans = 1
-            else:
-                ans = 0
-        return ans
+        if k<=2**(n-2):
+            return self.kthGrammar(n-1,k)
+        else:
+            return 1 - self.kthGrammar(n-1,k-2**(n-2))
         
