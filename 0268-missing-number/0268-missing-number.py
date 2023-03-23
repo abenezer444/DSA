f31424e6d211a05bb0dfdd1e5596305b7cf54818
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2023-03-23 19:41:41 +0300

    Time: 92 ms (95.61%), Space: 15.1 MB (15.44%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,7 +1,12 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        numbers=set(nums)
-        for i in range(len(nums)+1):
-            if i not in numbers:
-                return i
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        total = 0
+        for i in range(1,len(nums)+1):
+            total += i
+        return total - sum(nums)
+            
         
