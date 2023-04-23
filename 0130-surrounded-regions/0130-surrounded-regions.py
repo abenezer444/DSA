133d2cc83b710ea92783f97e3606034c92e9a9ce
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2023-04-23 21:46:35 +0300

    Time: 148 ms (29.30%), Space: 16.1 MB (30.26%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,23 +3,17 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        
         saviours = set()
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         visited = [[0 for i in range(len(grid[0]))] for j in range(len(grid))]
-        
-       
-            
         
         def onBorder(row,col):
             if inbound:
                 return (row == 0 or row == len(grid) - 1) or (col == 0 or col == len(grid[0]) - 1)
             return False
-        
-            
+          
         def inbound(row, col):
             return (0 <= row < len(grid) and 0 <= col < len(grid[0]))
-        
         
         def dfs(grid, visited, row, col):
             
@@ -37,6 +31,7 @@ class Solution:
                         saviours.add((new_row,new_col))
                         
                         dfs(grid, visited, new_row, new_col)
+                        
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 'O' and onBorder(i,j):
@@ -44,8 +39,10 @@ class Solution:
         
         for saviour in list(saviours):
             dfs(grid,visited,saviour[0],saviour[1])
+            
         for i in range(len(grid)):
             for j in range(len(grid[0])):
+                
                 if (i,j) not in saviours:
                     grid[i][j] = 'X'
         
