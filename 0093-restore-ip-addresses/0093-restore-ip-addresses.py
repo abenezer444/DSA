4d771a4e210261f2e593de75fadb4931c1e8fd26
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2023-04-24 21:50:01 +0300

    Time: 42 ms (36.78%), Space: 13.8 MB (70.01%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,29 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        output = []
+        
+        def validate(start, end):
+            if int(s[start:end]) <= 255:
+                if len(s[start:end]) > 1 and s[start] != "0" or len(s[start:end]) == 1:
+                    return True
+            return False
+            
+        def backtrack(cur, start):
+            if len(cur) == 4:
+                if start >= len(s):
+                    output.append(".".join(cur))
+                return
+
+            for idx in range(start, len(s)):
+                if validate(start, idx+1):
+                    cur.append(s[start:idx+1])
+                    backtrack(cur, idx+1)
+                    cur.pop()
+                else:
+                    return 
+            return 
+    
+        for idx in range(len(s)):
+            if validate(0, idx+1):
+                backtrack([s[:idx+1]], idx + 1)
+        return output
