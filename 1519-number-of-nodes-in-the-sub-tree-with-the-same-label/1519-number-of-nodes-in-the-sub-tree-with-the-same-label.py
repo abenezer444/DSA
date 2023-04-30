470f3f56434537b0dd1a9438e2de7c21f3bf71ae
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2023-04-30 10:46:18 +0300

    Time: 3091 ms (16.78%), Space: 205.6 MB (17.95%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.py
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.py
@@ -2,25 +2,30 @@ class Solution:
     def countSubTrees(self, n: int, edges: List[List[int]], labels: str) -> List[int]:
 
         ans = [0] * n 
-        adj = defaultdict(list)
-        seen = defaultdict(int)
+        graph = defaultdict(list)
+        visited = set()
+    
 
         for edge in edges:
-            adj[edge[0]].append(edge[1])
-            adj[edge[1]].append(edge[0])
+            graph[edge[0]].append(edge[1])
+            graph[edge[1]].append(edge[0])
         
 
-        def dfs(curr,parent):
-            prev = seen[labels[curr]]
+        def dfs(node):
+            count = Counter(labels[node])
+            visited.add(node)
             
-            for i in adj[curr]:
-                if i != parent:
-                    dfs(i,curr)
+            for neighbour in graph[node]:
+                if neighbour not in visited:
+                    count += dfs(neighbour)
+            
+            ans[node] = count[labels[node]]
+            return count
+                
 
-            seen[labels[curr]] += 1
-            ans[curr] = seen[labels[curr]] - prev
+           
 
 
-        dfs(0,-inf)
+        dfs(0)
 
         return ans
