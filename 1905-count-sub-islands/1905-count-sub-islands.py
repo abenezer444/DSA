e521995dca313fd36a0a86bc65111a6c6741dc7e
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2023-04-30 22:27:41 +0300

    Time: 3203 ms (46.16%), Space: 111.8 MB (11.25%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -1,26 +1,32 @@
 class Solution:
-    def countSubIslands(self, grid1: List[List[int]], grid: List[List[int]]) -> int:
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        visited = [[0 for i in range(len(grid[0]))] for j in range(len(grid))]
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
         
-        res = 0
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        visited = set()
+        subIslands = 0
+        
         def inbound(row, col):
-            return (0 <= row < len(grid) and 0 <= col < len(grid[0]))
+            return (0 <= row < len(grid2) and 0 <= col < len(grid2[0]))
+        
         def dfs(row, col):
-            if not visited[row][col] and grid[row][col] == 1:
-                if  grid1[row][col] == 0:
-                    self.t = False
-                visited[row][col] = True
-                for row_change, col_change in directions:
-                    new_row = row + row_change
-                    new_col = col + col_change
-                    if inbound(new_row, new_col):
+            visited.add((row,col))
+            grid2[row][col] = 0
+            for row_change, col_change in directions:
+                new_row = row + row_change
+                new_col = col + col_change
+                if inbound(new_row, new_col) and grid2[new_row][new_col] == 1:
                         dfs(new_row, new_col)
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1 and not visited[i][j]:
-                    self.t = True
+                                      
+        for i in range(len(grid2)):
+            for j in range(len(grid2[0])):
+                if grid2[i][j] == 1 and grid1[i][j] == 0 and (i,j) not in visited:
                     dfs(i, j)
-                    if self.t:
-                        res += 1 
-        return res
+        
+        for i in range(len(grid2)):
+            for j in range(len(grid2[0])):
+                if grid2[i][j] == 1 and (i,j) not in visited:
+                    dfs(i,j)
+                    subIslands+=1
+        
+                    
+        return subIslands
