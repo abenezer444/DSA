58a027bda9e31bd7587117b8eae8e0de211568b4
abenezer444 <105964367+abenezer444@users.noreply.github.com>
2023-05-08 08:52:21 +0300

    Time: 738 ms (23.62%), Space: 57.4 MB (20.30%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
@@ -1,28 +1,25 @@
 class Solution:
     def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
-        def dfs(source, visited, adj, path):
-            nonlocal unique
-            path.append(source)
-            # print(path)
-            if hasApple[source]:
-                for i in range(len(path)-1,0,-1):
-                    if (path[i-1], path[i]) not in unique:
-                        unique.add((path[i-1], path[i]))
-                    else:
-                        break
-            for nbr in adj[source]:
-                if nbr not in visited:
-                    visited.add(nbr)
-                    dfs(nbr, visited, adj, path.copy())
-        path = []
-        unique = set()
-        adj = defaultdict(list)
+        graph = defaultdict(list)
         for edge in edges:
-            u, v = edge
-            adj[u] += [v]
-            adj[v] += [u]
-        visited = set()
-        visited.add(0)
-        dfs(0, visited, adj, path)
+            graph[edge[0]].append(edge[1])
+            graph[edge[1]].append(edge[0])
        
-        return 2 * len(unique)
+        visited = set()  
+        def dfs(node):
+            time = 0
+            visited.add(node)
+            
+            for child in graph[node]:
+                
+                if child in visited:
+                    continue
+                childTime = dfs(child)
+                
+                if childTime or hasApple[child]:
+                    time += 2 + childTime
+            
+            
+            return time
+        return dfs(0)
+        
