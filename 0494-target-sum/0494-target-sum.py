eea4ef2c03e0427a67d862491b669e903d59e85a
abenezer <105964367+abenezer444@users.noreply.github.com>
2023-07-14 18:51:39 +0300

    Time: 362 ms (45.91%), Space: 37.9 MB (23.97%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,10 +1,13 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        @cache
-        def recur(index,current):
-            if index==len(nums):
-                if current==target:
+        memo = {}
+        def recur(index, current):
+            if (index, current) in memo:
+                return memo[(index, current)]
+            if index == len(nums):
+                if current == target:
                     return 1
                 return 0
-            return recur(index+1,current+nums[index])+recur(index+1,current-nums[index])
+            memo[(index, current)] = recur(index + 1, current + nums[index]) + recur(index + 1, current - nums[index])
+            return memo[(index, current)]
         return recur(0,0)
