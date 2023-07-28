aaca344507231818be5eeb456ab4da9b37e3f187
abenezer <105964367+abenezer444@users.noreply.github.com>
2023-07-28 15:13:24 +0300

    Time: 67 ms (98.46%), Space: 19.9 MB (6.87%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,12 +1,16 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        @cache
+        memo = {}
         def recur(row,col):
+            if (row,col) in memo:
+                return memo[(row,col)]
             if row == len(triangle) or col == len(triangle[-1]):
                 return 0
             minn = 0
             goDown = minn + recur(row+1,col)
             goDownRight = minn + recur(row+1,col+1)
             
-            return triangle[row][col] + min(goDown,goDownRight)
+            memo[(row,col)] = triangle[row][col] + min(goDown,goDownRight)
+            
+            return memo[(row,col)]
         return recur(0,0)
