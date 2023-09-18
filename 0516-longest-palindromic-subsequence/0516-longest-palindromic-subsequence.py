d38a8686b9d93317fbe201da7febb2f4a835046b
abenezer <105964367+abenezer444@users.noreply.github.com>
2023-09-18 11:55:22 +0300

    Time: 867 ms (86.19%), Space: 252.5 MB (13.75%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -1,15 +1,18 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        @cache
+        cache =  {}
         def recur(left,right):
+            if (left,right) in cache:
+                return cache[(left,right)]
          
             if right <  left:
                 return 0
             elif left == right:
                 return 1
             elif s[left] == s[right]:
-                return 2  + recur(left + 1, right - 1)
+                cache[(left,right)] = 2  + recur(left + 1, right - 1)
             else:
-                return max(recur(left + 1,right),recur(left,right - 1))
+                cache[(left,right)] =  max(recur(left + 1,right),recur(left,right - 1))
+            return cache[(left,right)]
         return recur(0,len(s) - 1)
         
