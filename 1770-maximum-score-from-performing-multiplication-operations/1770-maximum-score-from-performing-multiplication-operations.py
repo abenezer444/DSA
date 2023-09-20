e3b2b1e9dcd7e7a057e563f09b10130fbcd934a0
abenezer <105964367+abenezer444@users.noreply.github.com>
2023-09-20 20:30:33 +0300

    Time: 1159 ms (46.72%), Space: 173.2 MB (9.95%) - LeetHub

---
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
+    
+        @cache
+        def recur(left,right,index):
+            if index >= len(multipliers):
+                return 0
+            if right == left:
+                return nums[left] * multipliers[index]
+            
+            
+            leftSum = (nums[left] * multipliers[index]) + recur(left + 1, right, index + 1)
+            rightSum = (nums[right] * multipliers[index]) + recur(left , right - 1, index + 1)
+            return max(leftSum,rightSum)
+        return recur(0,len(nums) - 1, 0)
+                
+                
+        
