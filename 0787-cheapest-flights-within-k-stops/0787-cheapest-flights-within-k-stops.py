545b6b980274b498257114ed3cb221f326fc74fa
abenezer <105964367+abenezer444@users.noreply.github.com>
2023-10-05 16:58:27 +0300

    Time: 207 ms (20.48%), Space: 19.8 MB (6.45%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -0,0 +1,42 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        
+        graph = defaultdict(list)
+        
+        for start,end,weight in flights:
+            graph[start].append([weight,end])
+        
+        
+        distances = {node:[float(inf),0] for node in range(n)}
+        
+        distances[src] = [0,0]
+        
+        heap = [[0,src,-1]]
+        
+        visited = set()
+        
+        while heap:
+          
+            
+            cost,node,step = heappop(heap)
+            if (node,step) in visited:
+                continue
+            
+            if node == dst and step <= k:
+                return cost
+            visited.add((node,step))
+           
+            
+            if node == dst and step <= k:
+                return cost
+            
+            for weight, child in graph[node]:
+               
+                if step < k:
+                    heappush(heap,[cost + weight,child,step + 1])
+        return -1
+            
+            
+            
+            
+        
