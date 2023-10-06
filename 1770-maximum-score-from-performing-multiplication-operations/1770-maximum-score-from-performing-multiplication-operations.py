9e96091c7ea46f8436f039546984b442286eab6b
abenezer <105964367+abenezer444@users.noreply.github.com>
2023-10-06 11:52:57 +0300

    Time: 882 ms (92.11%), Space: 27.9 MB (80.73%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.py
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.py
@@ -1,20 +1,12 @@
 class Solution:
     def maximumScore(self, nums: List[int], multipliers: List[int]) -> int:
-    
-        dp = {}
-        def recur(left,right,index):
-            if (left,right) in dp:
-                return dp[(left,right)]
-            if index >= len(multipliers):
-                return 0
-            
-            
-            
-            leftSum = (nums[left] * multipliers[index]) + recur(left + 1, right, index + 1)
-            rightSum = (nums[right] * multipliers[index]) + recur(left , right - 1, index + 1)
-            dp[(left,right)] = max(leftSum,rightSum)
-            return dp[(left,right)]
-        return recur(0,len(nums) - 1, 0)
-                
-                
+        n, m = len(nums), len(multipliers)
+        dp = [[0] * (m + 1) for _ in range(m + 1)]
         
+        for i in range(m - 1, -1, -1):
+            for left in range(i, -1, -1):
+                mult = multipliers[i]
+                right = n - 1 - (i - left)
+                dp[i][left] = max(mult * nums[left] + dp[i + 1][left + 1], 
+                                  mult * nums[right] + dp[i + 1][left])        
+        return dp[0][0]
